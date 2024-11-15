82748ddffd196041c17a5cc8aa9fc1d84ea8d991
ash9river <ash9river@gmail.com>
2024-11-15 21:03:19 +0900

    보여주기용 임시 저장용

--- src/Components/KakaoMap/MapContainer.tsx
+++ src/Components/KakaoMap/MapContainer.tsx
@@ -1,4 +1,4 @@
-import { Map } from "react-kakao-maps-sdk";
+import { Map, ZoomControl } from "react-kakao-maps-sdk";
 import useKakaoLoader from "../../Hooks/useKakaoLoader";
 import { useEffect, useState } from "react";
 import useGeolocation from "../../Hooks/useGeolocation";
@@ -36,7 +36,23 @@ function MapContainer() {
       }}
       level={6} // 지도의 확대 레벨
       onCreate={setMap}
-    />
+    >
+      {/* queryResult.map((position: any, index: number) => (
+        <MapMarker
+          key={`${position.title}-${position.latlng}-${index}`}
+          position={position.latlng} // 마커를 표시할 위치
+          image={{
+            src: "https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png", // 마커이미지의 주소입니다
+            size: {
+              width: 24,
+              height: 35,
+            }, // 마커이미지의 크기입니다
+          }}
+          title={position.title} // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
+        />
+      )) */}
+      <ZoomControl position="BOTTOMRIGHT" />
+    </Map>
   );
 }
 
