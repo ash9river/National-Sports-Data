7d2780e288829c68d09e1e84c4c10ad8445eebf0
ash9river <121378532+ash9river@users.noreply.github.com>
2024-12-08 21:30:44 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist',
-  },
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://national-sports-data.site',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      },
-    },
-  },
+  base: '/', // 기본 경로 설정
 });
